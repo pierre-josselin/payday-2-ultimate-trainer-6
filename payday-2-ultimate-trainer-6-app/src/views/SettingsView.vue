6b8629700336bddd45a1dd6845a87ba180ff6f28
Pierre Josselin <contact@pierre-josselin.fr>
2023-07-08 10:40:01 +0200

    Add locale setting

--- payday-2-ultimate-trainer-6-app/src/views/SettingsView.vue
+++ payday-2-ultimate-trainer-6-app/src/views/SettingsView.vue
@@ -9,6 +9,7 @@ export default {
     },
     data() {
         return {
+            locale: null,
             theme: null,
             themes: [
                 "darkly",
@@ -42,9 +43,13 @@ export default {
     created() {
         this.settingsStore = useSettingsStore();
 
+        this.locale = this.settingsStore.getSetting("locale") || this.$i18n.locale;
         this.theme = this.settingsStore.getSetting("theme");
     },
     methods: {
+        setLocale(locale) {
+            this.$i18n.locale = locale;
+        },
         setTheme() {
             this.settingsStore.setSetting("theme", this.theme);
             this.settingsStore.saveSettings();
@@ -60,6 +65,12 @@ export default {
         <div class="card">
             <div class="card-header">{{ $t("main.settings") }}</div>
             <div class="card-body p-4">
+                <div class="mb-3">
+                    <label for="locale" class="form-label">{{ $t("main.language") }}</label>
+                    <select id="locale" v-model="locale" class="form-select" @change="setLocale">
+                        <option value="en">English</option>
+                    </select>
+                </div>
                 <div>
                     <label for="theme" class="form-label">{{ $t("main.app_theme") }}</label>
                     <select id="theme" v-model="theme" class="form-select" @change="setTheme">
