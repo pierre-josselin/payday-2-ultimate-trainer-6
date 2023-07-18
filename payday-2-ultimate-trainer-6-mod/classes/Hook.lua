7904c870ea2e12b6189166409bcfd9196a3a1362
Pierre Josselin <contact@pierre-josselin.fr>
2023-07-18 21:03:14 +0200

    Fix error with vehicles to load

--- payday-2-ultimate-trainer-6-mod/classes/Hook.lua
+++ payday-2-ultimate-trainer-6-mod/classes/Hook.lua
@@ -50,28 +50,30 @@ function UT.Hook:gameSetup()
     table.insert(packages, "levels/instances/unique/hlm_vault/world")
     table.insert(packages, "levels/instances/unique/san_box001/world")
 
-    if UT.Utility:inTable("sport-car", UT:getSetting("vehicles-to-load")) then
-        table.insert(packages, "levels/narratives/bain/cage/world/world")
-    end
+    if UT:getSetting("vehicles-to-load") then
+        if UT.Utility:inTable("sport-car", UT:getSetting("vehicles-to-load")) then
+            table.insert(packages, "levels/narratives/bain/cage/world/world")
+        end
 
-    if UT.Utility:inTable("muscle-car", UT:getSetting("vehicles-to-load")) or UT.Utility:inTable("forklift", UT:getSetting("vehicles-to-load")) then
-        table.insert(packages, "levels/narratives/vlad/shout/world/world")
-    end
+        if UT.Utility:inTable("muscle-car", UT:getSetting("vehicles-to-load")) or UT.Utility:inTable("forklift", UT:getSetting("vehicles-to-load")) then
+            table.insert(packages, "levels/narratives/vlad/shout/world/world")
+        end
 
-    if UT.Utility:inTable("bike", UT:getSetting("vehicles-to-load")) then
-        table.insert(packages, "levels/narratives/elephant/born/world/world")
-    end
+        if UT.Utility:inTable("bike", UT:getSetting("vehicles-to-load")) then
+            table.insert(packages, "levels/narratives/elephant/born/world/world")
+        end
 
-    if UT.Utility:inTable("truck", UT:getSetting("vehicles-to-load")) then
-        table.insert(packages, "levels/narratives/vlad/jolly/world/world")
-    end
+        if UT.Utility:inTable("truck", UT:getSetting("vehicles-to-load")) then
+            table.insert(packages, "levels/narratives/vlad/jolly/world/world")
+        end
 
-    if UT.Utility:inTable("golf-cart", UT:getSetting("vehicles-to-load")) then
-        table.insert(packages, "levels/narratives/mcshay/ranc/world/world")
-    end
+        if UT.Utility:inTable("golf-cart", UT:getSetting("vehicles-to-load")) then
+            table.insert(packages, "levels/narratives/mcshay/ranc/world/world")
+        end
 
-    if UT.Utility:inTable("boat", UT:getSetting("vehicles-to-load")) then
-        table.insert(packages, "levels/narratives/pbr/jerry/world/world")
+        if UT.Utility:inTable("boat", UT:getSetting("vehicles-to-load")) then
+            table.insert(packages, "levels/narratives/pbr/jerry/world/world")
+        end
     end
 
     for index, value in pairs(packages) do
