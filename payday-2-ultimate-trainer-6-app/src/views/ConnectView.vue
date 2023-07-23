4bd2fd809054fa09e856bc61480d0e733c954c88
Pierre Josselin <contact@pierre-josselin.fr>
2023-07-23 13:29:16 +0200

    Fix connect view responsive

--- payday-2-ultimate-trainer-6-app/src/views/ConnectView.vue
+++ payday-2-ultimate-trainer-6-app/src/views/ConnectView.vue
@@ -67,29 +67,27 @@ export default {
     </div>
 
     <div class="d-flex align-items-center justify-content-center h-100">
-        <div class="p-4">
-            <div style="min-width: 450px;" class="card">
-                <div class="card-header d-flex justify-content-between align-items-center">
-                    <span>PAYDAY 2 - Ultimate Trainer 6</span>
-                    <a href="#" data-bs-toggle="modal" data-bs-target="#about-modal">{{ $t("main.about") }}</a>
-                </div>
-                <div class="card-body">
-                    <h5 class="card-title">{{ $t("main.server_connection") }}</h5>
-                    <form @submit.prevent="connect">
-                        <div class="mb-3">
-                            <label for="host" class="form-label">{{ $t("main.host") }}</label>
-                            <input id="host" v-model="host" type="text" maxlength="255" class="form-control" :disabled="connecting" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="port" class="form-label">{{ $t("main.port") }}</label>
-                            <input id="port" v-model="port" type="number" min="0" max="65535" step="1" class="form-control" :disabled="connecting" required>
-                        </div>
-                        <button type="submit" class="btn btn-primary w-100" :disabled="connecting">
-                            <span v-if="connecting" class="spinner-border spinner-border-sm" />
-                            <template v-else>{{ $t("main.connect") }}</template>
-                        </button>
-                    </form>
-                </div>
+        <div style="width: 100%; max-width: 450px;" class="card m-4">
+            <div class="card-header d-flex justify-content-between align-items-center">
+                <span>PAYDAY 2 - Ultimate Trainer 6</span>
+                <a href="#" data-bs-toggle="modal" data-bs-target="#about-modal">{{ $t("main.about") }}</a>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">{{ $t("main.server_connection") }}</h5>
+                <form @submit.prevent="connect">
+                    <div class="mb-3">
+                        <label for="host" class="form-label">{{ $t("main.host") }}</label>
+                        <input id="host" v-model="host" type="text" maxlength="255" class="form-control" :disabled="connecting" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="port" class="form-label">{{ $t("main.port") }}</label>
+                        <input id="port" v-model="port" type="number" min="0" max="65535" step="1" class="form-control" :disabled="connecting" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary w-100" :disabled="connecting">
+                        <span v-if="connecting" class="spinner-border spinner-border-sm" />
+                        <template v-else>{{ $t("main.connect") }}</template>
+                    </button>
+                </form>
             </div>
         </div>
     </div>
