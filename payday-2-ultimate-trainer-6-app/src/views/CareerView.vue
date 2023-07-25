0cb5a1054e2387bff06af58c6be95659f4b542d2
Pierre Josselin <contact@pierre-josselin.fr>
2023-07-25 23:52:09 +0200

    Update CareerView.vue

--- payday-2-ultimate-trainer-6-app/src/views/CareerView.vue
+++ payday-2-ultimate-trainer-6-app/src/views/CareerView.vue
@@ -33,13 +33,13 @@ export default {
             perkPoints: null,
             blackMarketCategory: "masks",
             selectAllBlackMarketItems: false,
-            blackMarketItemsSearch: null,
+            blackMarketItemSearch: null,
             selectedBlackMarketItems: [],
             selectAllTrophies: false,
-            trophiesSearch: null,
+            trophySearch: null,
             selectedTrophies: [],
             selectAllSteamAchievements: false,
-            steamAchievementsSearch: null,
+            steamAchievementSearch: null,
             selectedSteamAchievements: [],
             blackMarketCategoryLocaleKeys: {
                 colors: "colors",
@@ -56,14 +56,14 @@ export default {
                 return null;
             }
 
-            if (!this.blackMarketItemsSearch) {
+            if (!this.blackMarketItemSearch) {
                 return this.blackMarketItems[this.blackMarketCategory];
             }
 
             const filteredBlackMarketItems = [];
             for (const blackMarketItemId of this.blackMarketItems[this.blackMarketCategory]) {
                 const blackMarketItemName = this.$t(`${this.blackMarketCategoryLocaleKeys[this.blackMarketCategory]}.${blackMarketItemId}`);
-                if (blackMarketItemName.toLowerCase().includes(this.blackMarketItemsSearch.toLowerCase())) {
+                if (blackMarketItemName.toLowerCase().includes(this.blackMarketItemSearch.toLowerCase())) {
                     filteredBlackMarketItems.push(blackMarketItemId);
                 }
             }
@@ -71,14 +71,14 @@ export default {
             return filteredBlackMarketItems;
         },
         filteredTrophies() {
-            if (!this.trophiesSearch) {
+            if (!this.trophySearch) {
                 return this.trophies;
             }
 
             const filteredTrophies = [];
             for (const trophyId of this.trophies) {
                 const trophyName = this.$t(`trophies.${trophyId}`);
-                if (trophyName.toLowerCase().includes(this.trophiesSearch.toLowerCase())) {
+                if (trophyName.toLowerCase().includes(this.trophySearch.toLowerCase())) {
                     filteredTrophies.push(trophyId);
                 }
             }
@@ -86,14 +86,14 @@ export default {
             return filteredTrophies;
         },
         filteredSteamAchievements() {
-            if (!this.steamAchievementsSearch) {
+            if (!this.steamAchievementSearch) {
                 return this.steamAchievements;
             }
 
             const filteredSteamAchievements = [];
             for (const steamAchievementId of this.steamAchievements) {
                 const steamAchievementName = this.$t(`steam_achievements.${steamAchievementId}`);
-                if (steamAchievementName.toLowerCase().includes(this.steamAchievementsSearch.toLowerCase())) {
+                if (steamAchievementName.toLowerCase().includes(this.steamAchievementSearch.toLowerCase())) {
                     filteredSteamAchievements.push(steamAchievementId);
                 }
             }
@@ -453,7 +453,7 @@ export default {
                                                                 <input v-model="selectAllBlackMarketItems" class="form-check-input" type="checkbox" role="button" @change="toggleAllBlackMarketItems">
                                                             </th>
                                                             <th class="bg-dark">
-                                                                <input v-model="blackMarketItemsSearch" type="search" class="form-control form-control-sm">
+                                                                <input v-model="blackMarketItemSearch" type="search" class="form-control form-control-sm">
                                                             </th>
                                                         </tr>
                                                     </thead>
@@ -491,7 +491,7 @@ export default {
                                                             <input v-model="selectAllTrophies" class="form-check-input" type="checkbox" role="button" @change="toggleAllTrophies">
                                                         </th>
                                                         <th class="bg-dark">
-                                                            <input v-model="trophiesSearch" type="search" class="form-control form-control-sm">
+                                                            <input v-model="trophySearch" type="search" class="form-control form-control-sm">
                                                         </th>
                                                     </tr>
                                                 </thead>
@@ -520,7 +520,7 @@ export default {
                                                         <th style="width: 40px;" class="bg-dark text-center align-middle">
                                                             <input v-model="selectAllSteamAchievements" class="form-check-input" type="checkbox" role="button" @change="toggleAllSteamAchievements">
                                                         </th>
-                                                        <th class="bg-dark"><input v-model="steamAchievementsSearch" type="search" class="form-control form-control-sm"></th>
+                                                        <th class="bg-dark"><input v-model="steamAchievementSearch" type="search" class="form-control form-control-sm"></th>
                                                     </tr>
                                                 </thead>
                                                 <tbody>
